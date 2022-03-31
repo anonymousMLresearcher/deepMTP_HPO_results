78b22e883472caa5215af708d09bcb49111a8647
anonymousMLresearcher <anonymous.ml.researcher@gmail.com>
2022-03-31 18:51:43 +0000

    updating app with a third tab, names other plots

--- streamlit_interface.py
+++ streamlit_interface.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 from scipy.interpolate import interp1d
 from scipy.stats import rankdata
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def get_list_of_settings(aggregate_option):
@@ -107,7 +108,7 @@ def get_ranking(
 
         results_per_dataset[mode] = {
             "rankings": rankings_arr,
-            "raw_ratings": raw_performance_arr,
+            "raw_performance_arr": raw_performance_arr,
             "end_points": end_point_per_HPO_method,
         }
 
@@ -178,10 +179,6 @@ def show_performance_plots(plot_cols, data_dict, color_per_HPO_method):
     for mode, hp_data in data_dict.items():
 
         fig = go.Figure()
-
-        # fig.update_layout(
-        #    autosize=False, width=2000, height=1000, paper_bgcolor="LightSteelBlue",
-        # )
 
         for hp_name, hp_dict in hp_data.items():
             hyperopt_runtime_arr_mean = hp_dict["raw_data"]["hyperopt_runtime_arr_mean"]
@@ -354,6 +351,7 @@ color_per_HPO_method = {
     "SMAC": "#AB63FA",
     "BOHB": "#FFA15A",
 }
+
 base_path = "streamlit_cached_data/"
 visualize_error = "area"
 error_type = "std"
@@ -365,7 +363,8 @@ dir_structure_dict = get_file_structure(base_path)
 st.sidebar.title("HPO benchmarks for DeepMTP")
 
 viz_option = st.sidebar.radio(
-    "Visualization mode:", ("Individual performance plots", "Aggregated ranking plots")
+    "Visualization mode:",
+    ("Individual performance plots", "Aggregated ranking plots", "other plots"),
 )
 
 if viz_option == "Individual performance plots":
@@ -440,7 +439,7 @@ if viz_option == "Individual performance plots":
     show_ranking_plots(rank_cols, data_dict, resolution, color_per_HPO_method)
 
 
-else:
+elif viz_option == "Aggregated ranking plots":
     base_path = "streamlit_cached_data/"
 
     HPO_names = ["hyperband", "random_search", "2_x_random_search", "SMAC", "BOHB"]
@@ -602,3 +601,260 @@ else:
     else:
         st.warning("No datasets or MTP settings selected")
 
+
+elif viz_option == "other plots":
+    base_path = "streamlit_cached_data/"
+
+    HPO_names = ["hyperband", "random_search", "2_x_random_search", "SMAC", "BOHB"]
+    info_per_dataset = [
+        {
+            "MTP_setting": "MLC",
+            "dataset_name": "bibtex",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MLC",
+            "dataset_name": "Corel5k",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MTR",
+            "dataset_name": "rf2",
+            "metric_name": "RMSE",
+            "metric_average": "micro",
+        },
+        {
+            "MTP_setting": "MTR",
+            "dataset_name": "scm1d",
+            "metric_name": "RMSE",
+            "metric_average": "micro",
+        },
+        {
+            "MTP_setting": "MTL",
+            "dataset_name": "dog",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MTL",
+            "dataset_name": "bird",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "MC",
+            "dataset_name": "movielens_100k",
+            "metric_name": "RMSE",
+            "metric_average": "micro",
+        },
+        {
+            "MTP_setting": "MC",
+            "dataset_name": "movielens_1M",
+            "metric_name": "RMSE",
+            "metric_average": "micro",
+        },
+        {
+            "MTP_setting": "DP",
+            "dataset_name": "ern",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+        {
+            "MTP_setting": "DP",
+            "dataset_name": "srn",
+            "metric_name": "aupr",
+            "metric_average": "macro",
+        },
+    ]
+
+    # aggregate selector
+    aggregate_option = st.sidebar.selectbox(
+        "Select a aggregation mode", ["Classification", "Regression",],
+    )
+
+    list_of_settings = get_list_of_settings(aggregate_option)
+
+    list_of_datasets = [
+        d for d in info_per_dataset if d["MTP_setting"] in list_of_settings
+    ]
+
+    metric_used = set([dataset["metric_name"] for dataset in list_of_datasets])
+    averaging_used = set([dataset["metric_average"] for dataset in list_of_datasets])
+
+    st.header(
+        "Calculating the difference on "
+        + list(averaging_used)[0]
+        + "-"
+        + list(metric_used)[0]
+    )
+
+    result_per_dataset = []
+    # get the ranking matrices from every dataset-metric combo
+    for dataset in list_of_datasets:
+
+        result_per_dataset.append(
+            get_ranking(
+                base_path=base_path,
+                dataset_option=dataset["dataset_name"],
+                metric_option=dataset["metric_name"],
+                averaging_option=dataset["metric_average"],
+                resolution=100,
+            )
+        )
+
+    averaged_average_ranking_per_mode = {}
+    # calculate the average ranking (separately for the validation and test sets)
+    for mode in ["val", "test"]:
+        averaged_average_ranking_per_hpo_method = []
+        # this is the idea of the padding option. It doesn't make sense as you lose temporal information. Sampling works better
+
+        ranking_per_dataset = np.array(
+            [d[mode]["rankings"] for d in result_per_dataset]
+        )
+        # st.write("Averaging over " + str(len(ranking_per_dataset)) + " datasets")
+        average_ranking = np.nanmean(ranking_per_dataset, axis=0)
+        # fig = go.Figure()
+        # fig.update_layout(
+        #     xaxis=dict(title="% of runtime"), yaxis=dict(title="ranking"),
+        # )
+
+        for i in range(average_ranking.shape[0]):
+            nan_ids = np.isnan(average_ranking[i, :])
+            # fig.add_trace(
+            #     go.Scatter(
+            #         x=np.arange(average_ranking.shape[1])[~nan_ids],
+            #         y=average_ranking[i, ~nan_ids],
+            #         mode="lines+markers",
+            #         name=HPO_names[i],
+            #         line_color=color_per_HPO_method[HPO_names[i]],
+            #     )
+            # )
+            averaged_average_ranking_per_hpo_method.append(
+                np.mean(average_ranking[i, ~nan_ids])
+            )
+            average_end_point = np.mean(
+                [d[mode]["end_points"][HPO_names[i]] for d in result_per_dataset]
+            )
+            # fig.add_vline(
+            #     x=average_end_point,
+            #     line_width=3,
+            #     line_dash="dash",
+            #     line_color=color_per_HPO_method[HPO_names[i]],
+            # )
+
+        averaged_average_ranking_per_mode[
+            mode
+        ] = averaged_average_ranking_per_hpo_method
+
+        # st.title(mode)
+        # st.plotly_chart(fig, use_container_width=True)
+        # st.markdown("""---""")
+
+    for mode in ["val", "test"]:
+        # isolate the raw values. The raw_ratings variable will have the following shape (num_datasets, num_HPO_methods, sampling resolution)
+        raw_performances = np.array(
+            [d[mode]["raw_performance_arr"] for d in result_per_dataset]
+        )
+
+        # calculate the mean of the raw_ratings_variable on the first axis-num_datasets. This will give the average rating values of every HPO method for across datasets (important!! use metrics that make sense for all datasets)
+        average_performances = np.nanmean(raw_performances, axis=0)
+        for i, val in enumerate(averaged_average_ranking_per_mode[mode]):
+            # print(HPO_names[i] + ": " + str(val))
+            pass
+
+        # isolate the names of the two top performing HPO methods (in terms of the average of average ranks)
+        top_HPO_method_names = [
+            HPO_names[i]
+            for i in np.argsort(averaged_average_ranking_per_mode[mode])[:2]
+        ]
+
+        # isolate the average ratings for only two top performing HPO methods
+        average_performances = average_performances[
+            np.argsort(averaged_average_ranking_per_mode[mode])[:2], :
+        ]
+
+        # replace any possible nan values with zeros to eliminate any errors during plotting
+        average_performances = np.nan_to_num(average_performances)
+        fig = plt.figure(figsize=(25, 10))
+
+        ax = plt.gca()
+        ax.set_title(mode, fontsize=30)
+
+        plt.rc("xtick", labelsize=35)
+        plt.rc("ytick", labelsize=25)
+
+        # calculate the score difference between the two methods
+        st.subheader(
+            "visualizing diff: "
+            + top_HPO_method_names[0]
+            + " - "
+            + top_HPO_method_names[1]
+        )
+        diff = average_performances[0] - average_performances[1]
+
+        # define the upper and lower limit of the y-axis
+        ax.set_ylim([-max(diff), max(diff)])
+        ax.set_xlim([0, 100])
+
+        # color the two quadrants
+        plt.fill_between(
+            [0, 100],
+            0,
+            max(diff),
+            alpha=0.2,
+            color=color_per_HPO_method[top_HPO_method_names[0]],
+        )
+        plt.fill_between(
+            [0, 100],
+            0,
+            -max(diff),
+            alpha=0.2,
+            color=color_per_HPO_method[top_HPO_method_names[1]],
+        )
+
+        # st.write(top_HPO_method_names[0] + ": " + str(average_performances[0]))
+        # st.write(top_HPO_method_names[1] + ": " + str(average_performances[1]))
+
+        ax.text(
+            0.5,
+            0.75,
+            top_HPO_method_names[0].capitalize(),
+            transform=ax.transAxes,
+            fontsize=60,
+            color="gray",
+            alpha=0.5,
+            ha="center",
+            va="center",
+            rotation=0,
+        )
+
+        ax.text(
+            0.5,
+            0.25,
+            top_HPO_method_names[1].capitalize(),
+            transform=ax.transAxes,
+            fontsize=60,
+            color="gray",
+            alpha=0.5,
+            ha="center",
+            va="center",
+            rotation=0,
+        )
+        if list(metric_used)[0] in ["hamming_loss", "RRMSE", "RMSE", "MAE", "MSE"]:
+            # st.write("Loss detected. Flipping values")
+            plt.plot(
+                np.arange(average_performances.shape[1]),
+                -diff,
+                linewidth=3,
+                color="black",
+            )
+        else:
+            plt.plot(
+                np.arange(average_performances.shape[1]),
+                diff,
+                linewidth=3,
+                color="black",
+            )
+        st.pyplot(fig)
